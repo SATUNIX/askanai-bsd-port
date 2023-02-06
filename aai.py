0189c1b4402ea3cb5c7e1a2e77b79bb776e802bc
Rory Linehan <rorylinehan@protonmail.com>
2023-02-06 17:17:04 -0600

    getting responses from github and openai

--- aai.py
+++ aai.py
@@ -2,14 +2,29 @@ import os
 import argparse
 import openai
 
+
 parser = argparse.ArgumentParser()
-parser.add_argument("--pr", help="review a PR")
-parser.add_argument("prompt", help="prompt for OpenAI", nargs="+")
+parser.add_argument("--pr", help="review a PR", required=False)
+parser.add_argument("--prompt", help="prompt for OpenAI", nargs="+", required=False)
 args = parser.parse_args()
-args.prompt = " ".join(args.prompt)
+
+if args.pr is not None:
+  from github import Github
+  import requests
+  g = Github(os.getenv("AAI_GITHUB_ACCESS_TOKEN"))
+  repo = "/".join(args.pr.split('/')[3:5])  # TODO: this is dumb, make it smart
+  pull = int(args.pr.split('/')[-1])  # TODO: dumb-ish
+  pr = g.get_repo(repo).get_pull(pull)
+  prompt_pr = ''
+  for file in pr.get_files():
+    contents = requests.get(file.raw_data['raw_url']).content
+    prompt_pr = prompt_pr + ' \n ' + contents.decode("utf-8")  # TODO: ew
+  args.prompt = 'Review the following code for any possible bugs, errors, or improvements, and provide a detailed summary of the findings with a simple explanation: ' + prompt_pr
+
+if args.prompt is not None:
+  args.prompt = " ".join(args.prompt)
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
-
 response = openai.Completion.create(
   model=os.getenv("OPENAI_MODEL"),
   prompt=args.prompt,

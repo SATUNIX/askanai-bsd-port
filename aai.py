2874c6be4b69d7454e5d6dd562e70183e4e27d99
Rory Linehan <rorylinehan@protonmail.com>
2023-02-06 21:15:03 -0600

    add tokenizer

--- aai.py
+++ aai.py
@@ -4,35 +4,39 @@ import openai
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--pr", help="review a PR", required=False)
-parser.add_argument("--prompt", help="prompt for OpenAI", nargs="+", required=False)
+parser.add_argument('--pr', help='review a PR', required=False)
+parser.add_argument('--prompt', help='prompt for OpenAI', nargs='+', required=False)
 args = parser.parse_args()
+
+
+max_tokens = int(os.getenv('OPENAI_MAX_TOKENS'))
 
 if args.pr is not None:
   import sys
+  from transformers import GPT2TokenizerFast
   from github import Github
-  g = Github(os.getenv("AAI_GITHUB_ACCESS_TOKEN"))
+  g = Github(os.getenv('AAI_GITHUB_ACCESS_TOKEN'))
   repo = '/'.join(args.pr.split('/')[3:5])  # TODO: this is dumb, make it smart
   pull = int(args.pr.split('/')[-1])  # TODO: dumb-ish
   pr = g.get_repo(repo).get_pull(pull)
   prompt_pr = ''
   for file in pr.get_files():
-    print(file.raw_data)
-    sys.exit(0)
-    patch = file.raw_data['']
-    prompt_pr = prompt_pr + ' \n ' + patch
-  args.prompt = 'Review the following patch changes for any possible bugs, errors, or improvements: ' + prompt_pr
+    prompt_pr = prompt_pr + ' \n ' + file.patch
+  args.prompt = 'Review the following patch changes for any possible bugs, errors, or improvements, and provide a summary of the findings: ' + prompt_pr
+  print(GPT2TokenizerFast.from_pretrained('gpt2')(args.prompt))
+  max_tokens = max_tokens - GPT2TokenizerFast.from_pretrained('gpt2')(args.prompt)
 
 if args.prompt is not None:
-  args.prompt = " ".join(args.prompt)
+  args.prompt = ' '.join(args.prompt)
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv('OPENAI_API_KEY')
 response = openai.Completion.create(
-  model=os.getenv("OPENAI_MODEL"),
+  model=os.getenv('OPENAI_MODEL'),
   prompt=args.prompt,
-  temperature=float(os.getenv("OPENAI_TEMPERATURE")),
-  top_p=float(os.getenv("OPENAI_TOP_P")),
-  frequency_penalty=float(os.getenv("OPENAI_FREQUENCY_PENALTY")),
-  presence_penalty=float(os.getenv("OPENAI_PRESENCE_PENALTY"))
+  max_tokens=max_tokens,
+  temperature=float(os.getenv('OPENAI_TEMPERATURE')),
+  top_p=float(os.getenv('OPENAI_TOP_P')),
+  frequency_penalty=float(os.getenv('OPENAI_FREQUENCY_PENALTY')),
+  presence_penalty=float(os.getenv('OPENAI_PRESENCE_PENALTY'))
 )
 print(response['choices'][0]['text'])
